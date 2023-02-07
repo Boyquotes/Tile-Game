a22f4f5af9088f9128fe63ec6498a1f77b997910
Gohard <chlebojad003@gmail.com>
2023-02-07 17:18:29 +0100

    save system

--- Resources/Save/Save.gd
+++ Resources/Save/Save.gd
@@ -1,10 +1,5 @@
 ### ----------------------------------------------------
 # Stores data regarding save
-# Save data is stored as follows:
-# - SAV_FOLDER_DIR (Folder)
-#  - SaveName      (Folder)
-#   - SaveName.res (Resource) [Stores save general data]
-#   - MapName.res  (Resource) [Stores edited map data]
 ### ----------------------------------------------------
 
 extends Resource
@@ -20,11 +15,8 @@ const SAV_FOLDER_DIR := "res://Temp/"
 # Name of this save
 export var SaveName := "Default"
 
-# Name of the current map
-export var CurrentMapName := "Default"
-
 # Map that is currently selected from MapList
-var _CurrentMap:MapSaveData
+var _MapSave:MapSaveData
 
 # Map template, not meant to be changed
 var _MapTemplate:MapSaveData
@@ -33,14 +25,15 @@ var _MapTemplate:MapSaveData
 # FUNCTIONS
 ### ----------------------------------------------------
 
-# Initializes the save
-func init_save(TileMaps:Array) -> bool:
-	var isOK = true
-	isOK = isOK and _load_MapTemplate(TileMaps, CurrentMapName)
-	isOK = isOK and _load_CurrentMap(TileMaps, CurrentMapName)
+
+func init_save(TileMaps:Array, mapName:String) -> bool:
+	var isOK := true
+	isOK = isOK and _load_MapTemplate(TileMaps, mapName)
+	if(not isOK): return isOK
+	isOK = isOK and _load_MapSave(TileMaps, mapName)
 	return isOK
 
-### ----------------------------------------------------
+### ---------------------------------------------------
 # Util
 ### ----------------------------------------------------
 
@@ -59,20 +52,19 @@ func _load_MapTemplate(TileMaps:Array, mapName:String) -> bool:
 	return true
 
 
-func _load_CurrentMap(TileMaps:Array, mapName:String) -> bool:
-	var mapPath := SAV_FOLDER_DIR + SaveName + "/" + mapName + ".res"
-	if(not LibK.Files.file_exist(mapPath)):
-		if (not _create_new_MapSave(TileMaps, mapPath, mapName)):
-			Logger.logErr(["Failed to load CurrentMap (unable to create new map): ", mapName], get_stack())
-			return false
-
+func _load_MapSave(TileMaps:Array, mapName:String) -> bool:
+	var mapPath := SAV_FOLDER_DIR + SaveName + ".res"
 	var TempRef = _load_map_resource(mapPath, TileMaps)
 	if(TempRef == null):
 		Logger.logErr(["Failed to load CurrentMap: ", mapName], get_stack())
 		return false
 	
-	_CurrentMap = null
-	_CurrentMap = TempRef
+	if(not TempRef.MapName == mapName):
+		Logger.logErr(["MapSave MapName doesnt match MapTemplate: ", TempRef.MapName, " ", mapName], get_stack())
+		return false
+
+	_MapSave = null
+	_MapSave = TempRef
 	
 	Logger.logMS(["Loaded CurrentMap successfully: ", mapName])
 	return true
