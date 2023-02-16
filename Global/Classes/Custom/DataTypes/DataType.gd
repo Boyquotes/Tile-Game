49cd4264f534d31ffa3863ca55b26879876a58bb
Gohard <chlebojad003@gmail.com>
2023-02-16 19:07:17 +0100

    documentation

--- Global/Classes/Custom/DataTypes/DataType.gd
+++ Global/Classes/Custom/DataTypes/DataType.gd
@@ -10,21 +10,23 @@ class_name DataType
 # FUNCTIONS
 ### ----------------------------------------------------
 
+
+# Creates a copy of DataType from its data string
 func from_str(s:String):
 	return from_array(str2var(s))
 
-
+# Creates copy of DataType data as string
 func _to_string() -> String:
 	return var2str(to_array())
 
-
+# Converts DataType data to an array
 func to_array() -> Array:
 	var arr := []
 	for propertyInfo in get_script().get_script_property_list():
 		arr.append(get(propertyInfo.name))
 	return arr
 
-
+# Creates copy of DataType data as Array
 func from_array(arr:Array):
 	var index := 0
 	for propertyInfo in get_script().get_script_property_list():
--- Global/Classes/Custom/Entities/GameEntity.gd
+++ Global/Classes/Custom/Entities/GameEntity.gd
@@ -2,7 +2,7 @@
 ### Desc
 ### ----------------------------------------------------
 
-extends DataType
+extends Node2D
 class_name GameEntity
 
 ### ----------------------------------------------------
@@ -22,3 +22,27 @@ func _set_MapPosition(posV3:Vector3):
 
 func save_entity():
 	pass
+
+
+# Creates a copy of entity from its data string
+func from_str(s:String):
+	return from_array(str2var(s))
+
+# Creates copy of entity data as string
+func _to_string() -> String:
+	return var2str(to_array())
+
+# Converts entity data to an array
+func to_array() -> Array:
+	var arr := []
+	for propertyInfo in get_script().get_script_property_list():
+		arr.append(get(propertyInfo.name))
+	return arr
+
+# Creates copy of entity data as Array
+func from_array(arr:Array):
+	var index := 0
+	for propertyInfo in get_script().get_script_property_list():
+		set(propertyInfo.name, arr[index])
+		index+=1
+	return self
